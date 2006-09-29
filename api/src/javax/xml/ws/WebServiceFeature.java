8a55bd7c9e75033a09c5cd8f683d4c5f1610736c
Doug Kohlert <doug.kohlert@sun.com>
2006-09-29 22:08:09 +0000

    added MUST have a unique ID to javadocs

--- api/src/javax/xml/ws/WebServiceFeature.java
+++ api/src/javax/xml/ws/WebServiceFeature.java
@@ -13,7 +13,12 @@ package javax.xml.ws;
  * The JAX-WS specification will define some standard features and
  * JAX-WS implementors are free to define additional features if
  * necessary.  Vendor specific features may not be portable so 
- * caution should be used when using them.
+ * caution should be used when using them. Each Feature definition 
+ * MUST define a <code>public static final String ID</code> 
+ * that can be used in the Feature annotation to refer 
+ * to the feature. This ID MUST be unique across all features
+ * of all vendors.  When defining a vendor specific feature ID,
+ * use a vendor specific namespace in the ID string. 
  *
  * @see javax.xml.ws.RespectBindingFeature
  * @see javax.xml.ws.soap.AddressingFeature
@@ -23,7 +28,7 @@ package javax.xml.ws;
  */
 public abstract class WebServiceFeature {
    /**
-    * Each Feature definition should define a public static final 
+    * Each Feature definition MUST define a public static final 
     * String ID that can be used in the Feature annotation to refer 
     * to the feature.
     */
